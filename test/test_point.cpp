6988715877d860ff43da5f2daba540a1f7f1f2c8
Edward Chou <prettyage.new@gmail.com>
2023-06-06 11:09:38 +0800

    Redefine point by using Eigen::Vector

--- test/test_point.cpp
+++ test/test_point.cpp
@@ -1,8 +1,8 @@
-#include "soil/geo/point.hpp"
 #include <algorithm> // for std::copy
 #include <iostream>
 #include <iterator> // for std::ostream_iterator
 #include <typeinfo>
+#include "soil/geo/point.hpp"
 
 template <typename T>
 std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
@@ -19,32 +19,37 @@ int main() {
 
     std::cout << "Create a point by using default constructor: ";
     soil::geo::Point<double, 5> p0;
-    for (size_t i=0; i<p0.dimension(); ++i) {
+    for (size_t i=0; i<p0.size(); ++i) {
         std::cout << p0[i] << ", ";
         p0[i] = i * 0.1 + 0.1;
     }
     std::cout << std::endl;
-    std::cout << "After setting: " << p0.to_vector() << std::endl << std::endl;
+    std::cout << "After setting: " << soil::geo::to_vector(p0)
+              << std::endl << std::endl;
 
-    auto p1 = p0.as<float>();
-    std::cout << "Change type to " << typeid(p1.x()).name() << std::endl;
-    p1.y(55.1f);
-    std::cout << "Second element is changed to " << p1.y() << std::endl << std::endl;
+    auto p1 = soil::geo::as<double, float, 5>(p0);
+    std::cout << "Change type to " << typeid(p1[0]).name() << std::endl;
+    p1[1] = 55.1f;
+    std::cout << "Second item changes to " << p1[1] << std::endl << std::endl;
 
-    soil::geo::FPoint3D p2(p1.to_vector());
-    std::cout << "Constructor 3D point by using vector: ";
-    for (size_t i = 0; i < p2.dimension(); ++i) {
-        std::cout << p2.at(i) << ", ";
-    }
-    std::cout << std::endl << std::endl;
+    auto p2 = soil::geo::from_vector<float, 3>(soil::geo::to_vector(p1));
+    std::cout << "Constructor 3D point by using vector: " << std::endl
+              << p2 << std::endl << std::endl;
 
     soil::geo::I32Point2D p3({76, 18});
     std::cout << "Constructor 2D integer point: ";
-    auto ptr = p3.ptr();
-    for (size_t i = 0; i < p3.dimension(); ++i) {
+    auto ptr = p3.data();
+    for (size_t i = 0; i < p3.size(); ++i) {
         std::cout << ptr[i] << ", ";
     }
     std::cout << std::endl << std::endl;
+
+    auto hp2 = soil::geo::to_homogeneous(p2);
+    std::cout << "Homogeneous point of P2: " << std::endl << hp2 << std::endl;
+    hp2[3] *= 10.0f;
+    std::cout << "Transformed point: " << std::endl
+              << soil::geo::from_homogeneous<float, 3>(hp2)
+              << std::endl << std::endl;
 
     return 0;
 }
